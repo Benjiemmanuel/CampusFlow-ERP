971a80b81c1323af890a7b1466c4b8cf2986d41a
Emmanuel Benjamin Akpan <emmanuelbenjamin451@gmail.com>
2026-07-05 11:07:17 +0100

    feat(core): implement university module and backend foundation

--- apps/api/src/modules/universities/university.controller.ts
+++ apps/api/src/modules/universities/university.controller.ts
@@ -1,73 +1,46 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response } from "express";
+
 import universityService from "./university.service";
 import { createUniversitySchema } from "./university.validation";
 
+import catchAsync from "../../shared/utils/catchAsync";
+import sendResponse from "../../shared/responses/sendResponse";
+
 class UniversityController {
-  /**
-   * Create University
-   */
-  async create(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    try {
-      const data = createUniversitySchema.parse(req.body);
+  create = catchAsync(async (req: Request, res: Response) => {
+    const data = createUniversitySchema.parse(req.body);
 
-      const university =
-        await universityService.createUniversity(data);
+    const university = await universityService.createUniversity(data);
 
-      return res.status(201).json({
-        success: true,
-        message: "University created successfully.",
-        data: university,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: "University created successfully.",
+      data: university,
+    });
+  });
 
-  /**
-   * Get All Universities
-   */
-  async getAll(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    try {
-      const universities =
-        await universityService.getUniversities();
+  getAll = catchAsync(async (req: Request, res: Response) => {
+    const universities = await universityService.getUniversities();
 
-      return res.status(200).json({
-        success: true,
-        data: universities,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      data: universities,
+    });
+  });
 
-  /**
-   * Get One University
-   */
-  async getById(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    try {
-      const university =
-        await universityService.getUniversity(req.params.id as string);
+  getById = catchAsync(async (req: Request, res: Response) => {
+    const university = await universityService.getUniversity(
+      req.params.id as string
+    );
 
-      return res.status(200).json({
-        success: true,
-        data: university,
-      });
-    } catch (error) {
-      next(error);
-    }
-  }
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      data: university,
+    });
+  });
 }
 
 export default new UniversityController();
